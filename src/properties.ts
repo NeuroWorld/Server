598f8489dd5b88340e2b28f16c31f3879d25ffaf
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2017-10-16 23:03:10 +0200

    Added update to reporter

--- src/properties.ts
+++ src/properties.ts
@@ -1,8 +1,8 @@
 export namespace Properties {
     export const ENTITY_SPEED: number = 0.1;
-    export const ENTITY_COUNT: number = 20;
+    export const ENTITY_COUNT: number = 10;
 
     export const WORLD_SIZE: number = 50;
     export const FIRE_CHANCE: number = 0.9999;
-    export const FOOD_CHANCE: number = 0.999;
+    export const FOOD_CHANCE: number = 0.99;
 }
